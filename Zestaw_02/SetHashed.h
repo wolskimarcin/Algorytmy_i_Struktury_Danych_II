fea53aeeb5c7189150dfc2db7f4b96d9283899d0
wolskimarcin <wolski.marcin.it@gmail.com>
2022-04-24 17:50:13 +0200

    Add set implementation with hashing

---
+++ Zestaw_02/SetHashed.h
@@ -0,0 +1,14 @@
+//
+// Created by busher on 21.04.22.
+//
+#include <iostream>
+
+#ifndef ZESTAW_02_SETHASHED_H
+#define ZESTAW_02_SETHASHED_H
+
+class SetHashed {
+
+};
+
+
+#endif //ZESTAW_02_SETHASHED_H
